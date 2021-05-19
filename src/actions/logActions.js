6ed58c4be52956277b68f8e6836208980b80b6d3
Rene-home-pc <15114388+RenV123@users.noreply.github.com>
2021-05-19 15:00:32 +0200

    Add a logs search function.

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -7,8 +7,26 @@ import {
   DELETE_LOG,
   SET_CURRENT,
   CLEAR_CURRENT,
+  SEARCH_LOGS,
 } from './types';
 import axios from 'axios';
+
+/**
+ * Search Logs
+ * @returns a async function
+ */
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+    const response = await axios.get(`/logs?q=${text}`);
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({ type: LOGS_ERROR, payload: error?.response.data });
+  }
+};
 
 /**
  * Get Logs from the server
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -7,6 +7,7 @@ import {
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_LOG,
+  SEARCH_LOGS,
 } from '../actions/types';
 
 const initialState = {
@@ -28,6 +29,11 @@ const logReducer = (state = initialState, action) => {
         ...state,
         logs: action.payload,
         loading: false,
+      };
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
       };
     case ADD_LOG:
       return {
